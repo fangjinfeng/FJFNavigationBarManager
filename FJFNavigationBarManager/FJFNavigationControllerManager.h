912b537f6a3983194302be5a8ba3d0540823a7af
fjf <116418179@qq.com>
2018-07-10 17:23:27 +0800

    1.修改注释

--- FJFNavigationBarManager/FJFNavigationControllerManager.h
+++ FJFNavigationBarManager/FJFNavigationControllerManager.h
@@ -13,31 +13,27 @@
 + (instancetype)sharedInstance;
 
 /**
- 生成 navigationController 并 设置 代理 为FJFNavigationControllerManager 单例
+ 设置 navigationController 遵循UINavigationControllerDelegate代理
+ 
+ @param navigationController 遵循UINavigationControllerDelegate代理的 navigationController
+ */
++ (void)setNavigationDelegateWithNavigationController:(UINavigationController *)navigationController;
+
+/**
+ 生成 navigationController 并 设置 UINavigationControllerDelegate代理 为FJFNavigationControllerManager 单例
  
  @param viewControllerName 根界面名称
  @return 生成 navigationController
  */
 + (UINavigationController *)navigationControllerWithViewControllerName:(NSString *)viewControllerName;
 /**
- 生成 navigationController 并 设置 代理 为FJFNavigationControllerManager 单例
+ 生成 navigationController 并 设置 UINavigationControllerDelegate代理 为FJFNavigationControllerManager 单例
 
  @param viewController 根界面
  @return 生成 navigationController
  */
 + (UINavigationController *)navigationControllerWithRootViewController:(UIViewController *)viewController;
 
-/**
- 设置 navigationController 遵循代理
-
- @param navigationController 遵循代理的 navigationController
- */
-+ (void)setNavigationDelegateWithNavigationController:(UINavigationController *)navigationController;
-
-- (void)checkNavigationBarHiddenFlagWhenWillDisapperNavigationController:(UINavigationController *)navigationController;
-
-// 更新 导航栏 状态
-- (void)updateNavigationBarStatusWithNavigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated;
 @end
 
 
